4ad0a6a9aba67658867a4f459006dc4e30824245
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-05-31 22:34:00 +0300

    add data generation script

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ PHONY: build
 
 build:
 	gcc src/knn.c -o bin/knn
+
+
+generate-data:
+	python ./scripts/generate_points.py --n 1000 --dim 512 --out ./data/dataset-1K-512.txt
---
+++ scripts/generate_points.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import numpy as np
+import typer
+
+
+def main(
+    n: int = typer.Option(default=1000),
+    dim: int = typer.Option(default=512),
+    out: Path = typer.Option(...),
+):
+    points = np.random.rand(n, dim)
+    with open(out, "w") as f:
+        for point in points:
+            f.write(" ".join(map(str, point)) + "\n")
+
+
+if __name__ == "__main__":
+    typer.run(main)
