f8f5c53c596eb74d5a2e3e074ae3c309216b89cc
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-06-08 14:52:16 +0300

    benchmark repetition

--- benchmark.sh
+++ benchmark.sh
@@ -7,7 +7,9 @@ args=(10 100 1000 10000)
 
 # Loop through each argument
 for arg in "${args[@]}"; do
-    # Run the command with the current argument and redirect the output to a file
-    make run-gpu ARGS="$arg" > "results/out-gpu-${arg}.txt"
-    make run-cpu ARGS="$arg" > "results/out-cpu-${arg}.txt"
+    for i in {1..5}; do
+        # Run the command with the current argument and redirect the output to a file
+        make run-gpu ARGS="$arg" > "results/out-gpu-${i}-${arg}.txt"
+        make run-cpu ARGS="$arg" > "results/out-cpu-${i}-${arg}.txt"
+    done
 done
