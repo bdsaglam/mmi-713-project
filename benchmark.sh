7de3fb14b31caa9e085be8d3e3004ad135a82d74
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-06-09 12:40:48 +0300

    re-benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -9,17 +9,16 @@ mkdir -p results
 
 # Define the array of arguments
 N_values=(10 100 1000 10000)
-Q_values=(10 100 1000)
+Q_values=(10 100 1000 10000)
 
 # Loop through each argument
 for N in "${N_values[@]}"; do
     for Q in "${Q_values[@]}"; do
-        q="$(min $N $Q)"
         for i in {1..3}; do
             echo "$i. $N $Q"
             # Run the command with the current argument and redirect the output to a file
-            make run-gpu ARGS="--N $N --Q $q" > "results/out-gpu-$N-$q-${i}.txt"
-            make run-cpu ARGS="--N $N --Q $q" > "results/out-cpu-$N-$q-${i}.txt"
+            make run-gpu ARGS="--N $N --Q $Q" > "results/out-gpu-$N-$Q-${i}.txt"
+            make run-cpu ARGS="--N $N --Q $Q" > "results/out-cpu-$N-$Q-${i}.txt"
         done
     done
 done
