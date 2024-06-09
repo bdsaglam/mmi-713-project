6707179b5f3cc682c76d2051bc8ee5e723cb1aa1
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-06-09 19:25:35 +0300

    update script

--- benchmark.sh
+++ benchmark.sh
@@ -4,8 +4,8 @@ min() {
     printf "%s\n" "$@" | sort -g | head -n1
 }
 
-rm -rf results
-mkdir -p results
+rm -rf tmp/results
+mkdir -p tmp/results
 
 # Define the array of arguments
 N_values=(10 100 1000 10000)
@@ -14,11 +14,11 @@ Q_values=(10 100 1000 10000)
 # Loop through each argument
 for N in "${N_values[@]}"; do
     for Q in "${Q_values[@]}"; do
-        for i in {1..3}; do
+        for i in {1..10}; do
             echo "$i. $N $Q"
             # Run the command with the current argument and redirect the output to a file
-            make run-gpu ARGS="--N $N --Q $Q" > "results/out-gpu-$N-$Q-${i}.txt"
-            make run-cpu ARGS="--N $N --Q $Q" > "results/out-cpu-$N-$Q-${i}.txt"
+            make run-gpu ARGS="--N $N --Q $Q" > "tmp/results/out-gpu-$N-$Q-${i}.txt"
+            make run-cpu ARGS="--N $N --Q $Q" > "tmp/results/out-cpu-$N-$Q-${i}.txt"
         done
     done
 done
